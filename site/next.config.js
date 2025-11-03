c84b900a2a56da1c75868c188c64e76d0cc435d4
SquirrelWorks LLC <squirrelworks@konderla.org>
2025-11-03 13:03:33 -0600

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   env: {
     name: 'SquirrelWorks Kasm Repository',
     description: 'SquirrelWorks 3rd Party Kasm Workspace Repository.',
-    icon: '/swlogo.png',
+    icon: 'https://squirrelworksllc.github.io/kasm-registry/1.1/swlogo.png',
     listUrl: 'https://squirrelworksllc.github.io/kasm-registry/',
     contactUrl: '',
   },
